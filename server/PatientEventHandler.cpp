d39de929008a9928a432a9e7205cba49279115a2
Kasper Nissen <kaspernissen@gmail.com>
2014-09-02 17:15:21 +0200

    PatientEventHandler implemented

--- server/PatientEventHandler.cpp
+++ server/PatientEventHandler.cpp
@@ -1,7 +1,11 @@
 #include "PatientEventHandler.h"
 #include "PatientEvent.h"
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <cstdlib>
 
+std::vector<std::string> x = split("one:two::three", ':');
 using namespace std;
 
 void PatientEventHandler::handleEvent(HANDLE handle)
@@ -12,7 +16,20 @@ void PatientEventHandler::handleEvent(HANDLE handle)
 
 Event* PatientEventHandler::parseEvent(HANDLE handle)
 {
-	//TODO: Get some stuff out of the handle and convert it to an AlarmEvent.
-	Event *test = new PatientEvent();
-	return test;
+	//Expected protocol "id;type;value"
+	string input = handle.receive();
+
+	std::istringstream ss(input);
+	std::string token;
+	std::vector<string> dataArray;
+
+	while (std::getline(ss, token, ';')) {
+		dataArray.push_back(token);
+	}
+
+	PatientEvent *event = new PatientEvent();
+	event->setValue(dataArray.pop_back);
+	//TODO: Set the type.
+
+	return event;
 }
