64996cdf612190b8ae0bf9757033c6628cfb7270
Martin Jensen <martin2610@gmail.com>
2014-09-02 16:25:57 +0200

    Tuple removed. DemuxTable header implemented.

--- framework/DemuxTable.h
+++ framework/DemuxTable.h
@@ -1,3 +1,6 @@
 class DemuxTable{
-
+public:
+	EventHandler* getHandler(unsigned int eventType);
+	void addHandler(unsigned int eventType, EventHandler* eh);
+	void removeHandler(EventHandler* eventHandler);	
 };
