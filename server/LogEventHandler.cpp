b6160cab37958b9286aacbefdd06042372dc9d66
Kasper Nissen <kaspernissen@gmail.com>
2014-09-02 16:32:24 +0200

    Eventhandler implemented. Parse handle methods missing

--- server/LogEventHandler.cpp
+++ server/LogEventHandler.cpp
@@ -1,11 +1,18 @@
 #include "LogEventHandler.h"
+#include "LogEvent.h"
+#include <iostream>
+
+using namespace std;
 
 void LogEventHandler::handleEvent(HANDLE handle)
 {
-
+	LogEvent* logEvent = static_cast<LogEvent*>(parseEvent(handle));
+	std::cout << "Text: " << logEvent->getText() << '\n';
 }
 
-Event LogEventHandler::parseEvent(HANDLE handle)
+Event* LogEventHandler::parseEvent(HANDLE handle)
 {
-
+	//TODO: Get some stuff out of the handle and convert it to an AlarmEvent.
+	Event *test = new LogEvent();
+	return test;
 }
