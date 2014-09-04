624c42dd609770352af15927fd5f8e48b56ab0b3
Martin Jensen <martin2610@gmail.com>
2014-09-04 14:50:03 +0200

    IT IS COMPILING

--- server/AlarmEventHandler.cpp
+++ server/AlarmEventHandler.cpp
@@ -14,6 +14,32 @@ void AlarmEventHandler::handleEvent(Handle* handle)
 Event* AlarmEventHandler::parseEvent(Handle* handle)
 {
 	//TODO: Get some stuff out of the handle and convert it to an AlarmEvent.
-	Event *test = new AlarmEvent();
-	return test;
+	
+	
+	//Expected protocol "id;comment;priority"
+	string input = handle->receive();
+
+	std::string delimiter = ";";
+	size_t pos = 0;
+	std::string token;
+	AlarmEvent *event = new AlarmEvent();
+	int count = 0;
+
+	while ((pos = input.find(delimiter)) != std::string::npos) {
+		token = input.substr(0, pos);
+		//type
+		if (count == 1)
+		{
+			event->setComment(token);
+		}
+
+		std::cout << token << std::endl;
+		input.erase(0, pos + delimiter.length());
+		count++;
+	}
+	//value
+	event->setPriority(input);
+
+	return event;
+	
 }
